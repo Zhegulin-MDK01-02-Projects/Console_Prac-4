a490ff2743d1cf66293541ee0faeeed10dcb3c60
MatveyZh404 <118770204+MatveyZh404@users.noreply.github.com>
2023-05-22 09:07:08 +0300

    Add files via upload

--- Консоль Практическое задание №4/Program.cs
+++ Консоль Практическое задание №4/Program.cs
@@ -1,56 +1,59 @@
-﻿static void Main(string[] args)
+﻿class Program
 {
-    int[,] matrix = { { 1, 2 }, { 3, 4 } };
-    int power = 3;
-
-    int[,] result = MatrixPower(matrix, power);
-
-    Console.WriteLine("Result:");
-    PrintMatrix(result);
-}
-
-static int[,] MatrixPower(int[,] matrix, int power)
-{
-    int[,] result = matrix;
-
-    for (int i = 1; i < power; i++)
+    static void Main(string[] args)
     {
-        result = MultiplyMatrices(result, matrix);
+        int[,] matrix = { { 1, 2 }, { 3, 4 } };
+        int power = 3;
+
+        int[,] result = MatrixPower(matrix, power);
+
+        Console.WriteLine("Result:");
+        PrintMatrix(result);
     }
 
-    return result;
-}
-
-static int[,] MultiplyMatrices(int[,] matrix1, int[,] matrix2)
-{
-    int rows = matrix1.GetLength(0);
-    int cols = matrix1.GetLength(1);
-
-    int[,] result = new int[rows, cols];
-
-    for (int i = 0; i < rows; i++)
+    static int[,] MatrixPower(int[,] matrix, int power)
     {
-        for (int j = 0; j < cols; j++)
+        int[,] result = matrix;
+
+        for (int i = 1; i < power; i++)
         {
-            result[i, j] = matrix1[i, j] * matrix2[i, j];
+            result = MultiplyMatrices(result, matrix);
         }
+
+        return result;
     }
 
-    return result;
-}
-
-static void PrintMatrix(int[,] matrix)
-{
-    int rows = matrix.GetLength(0);
-    int cols = matrix.GetLength(1);
-
-    for (int i = 0; i < rows; i++)
+    static int[,] MultiplyMatrices(int[,] matrix1, int[,] matrix2)
     {
-        for (int j = 0; j < cols; j++)
+        int rows = matrix1.GetLength(0);
+        int cols = matrix1.GetLength(1);
+
+        int[,] result = new int[rows, cols];
+
+        for (int i = 0; i < rows; i++)
         {
-            Console.Write(matrix[i, j] + " ");
+            for (int j = 0; j < cols; j++)
+            {
+                result[i, j] = matrix1[i, j] * matrix2[i, j];
+            }
         }
 
-        Console.WriteLine();
+        return result;
+    }
+
+    static void PrintMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+
+            Console.WriteLine();
+        }
     }
 }
